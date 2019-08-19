ea4a780d58b099334c9fe8c45c1a6a8ea4254294
bioinfonerd <johnsonnathant@gmail.com>
2019-08-19 14:03:36 -0400

    install example dataset

---
+++ install_example_dataset.sh
@@ -0,0 +1,4 @@
+#Download example data for testing pipeline
+
+wget --output-document=example_data.tar.gz https://www.dropbox.com/s/x0m1hc5m0we8bfv/example_data.tar.gz?dl=0
+tar -zxf example_data.tar.gz
